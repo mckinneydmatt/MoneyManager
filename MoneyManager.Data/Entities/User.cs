f2d0bbe9738edbd246b1bdfc32c8318cd1bd9648
mdskinner31 <mdskinner31@gmail.com>
2021-04-21 11:16:20 -0400

    SavingsAcct Commit

--- MoneyManager.Data/Entities/User.cs
+++ MoneyManager.Data/Entities/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,19 @@ namespace MoneyManager.Data.Entities
 {
     public class User
     {
+         
+        [Key]
+        public int UserAcctNumber { get; set; }
+        [Required]
+        public Guid UserID { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string PhoneNumber { get; set; }
+        [Required]
+        public string Address { get; set; }
+        public double GoalAmount { get; set; }
+        public double LiquidNetWorth { get; set; }
     }
 }
+
---
+++ MoneyManager.Services/SavingsAcctService.cs
@@ -0,0 +1,142 @@
+﻿using MoneyManager.Data;
+using MoneyManager.Data.Entities;
+using MoneyManager.Models;
+using MoneyManager.Models.Savings;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoneyManager.Services
+{
+    public class SavingsAcctService
+    {
+
+        private readonly Guid _userId;
+
+        public SavingsAcctService(Guid userId)
+        {
+            _userId = userId;
+        }
+
+        public bool CreateSavingsAcct(SavingsAcctCreate model)
+        {
+            var entity = new SavingsAcct()
+            {
+                AccountId = model.AccountId,
+
+                UserAccountNumber = model.UserAccountNumber,
+
+                SvAcctName = model.SvAcctName,
+
+                SvAcctBalance = model.SvAcctBalance
+            };
+
+            using (var ctx = new ApplicationDbContext())
+            {
+                ctx.SavingsAccts.Add(entity);
+                return ctx.SaveChanges() == 1;
+
+            }
+
+        }
+
+        public IEnumerable<SavingsAcctListItem> GetSavingsAcct()
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+
+                        .SavingsAccts
+                        .Where(e => e.AccountId == e.AccountId)
+                        .Select(
+                            e =>
+                                new SavingsAcctListItem
+                                {
+
+
+                                    AccountId = e.AccountId,
+
+                                    UserAccountNumber = e.UserAccountNumber,
+
+                                    SvAcctName = e.SvAcctName,
+
+                                    SvAcctBalance = e.SvAcctBalance
+
+                                }
+                        );
+
+                return query.ToArray();
+            }
+        }
+
+        public bool UpdateSavingsAcct(SavingsAcctEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                      
+                       .SavingsAccts
+                        .Single(e => e.AccountId == model.AccountId);
+
+                                    entity.AccountId = model.AccountId;
+
+                                    entity.UserAccountNumber = model.UserAccountNumber;
+
+                                    entity.SvAcctName = model.SvAcctName;
+
+                                    entity.SvAcctBalance = model.SvAcctBalance;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+
+        public SavingsAcctDetail GetSavingsAcctByAcctNumber(int UserAccountNumber)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        
+                        .SavingsAccts
+                        .Single
+                        (e => e.UserAccountNumber == UserAccountNumber);
+                
+                return
+                    new SavingsAcctDetail
+                    { 
+                        AccountId = entity.AccountId,
+
+                        UserAccountNumber = entity.UserAccountNumber,
+
+                        SvAcctName = entity.SvAcctName,
+
+                        SvAcctBalance = entity.SvAcctBalance
+
+
+                    };
+            }
+        }
+
+        public bool DeleteSavingsAcct(int UserAccountNumber)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                        
+                        .SavingsAccts
+                        .Single(e => e.UserAccountNumber == UserAccountNumber);
+
+                ctx.SavingsAccts.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
+            }
+
+
+        }
+    }
+}
